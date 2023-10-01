c7831a9aedc6c3c0467a2a43df54dd48ffb59430
hyedi3 <hyeri9918as@naver.com>
2023-10-01 23:46:14 +0900

    Add exam01 - println

---
+++ Java/src/exam01.java
@@ -0,0 +1,8 @@
+//exam01 class 정의 (프로젝트 파일 이름과 동일해야함)
+public class exam01 {
+
+    public static void main(String[] args) {
+        // 콘솔 창에 글자를 출력하는 코드
+        System.out.println("안드로이드를 위한 Java 연습");
+    }
+}
